882f5135c9315c62ae3ac4e83f0fa67c4e23a0dc
Fernando Segovia <fsegovia.act@gmail.com>
2023-05-23 14:25:17 -0300

    create form to comment post

--- src/Security/AppAthenticatorAuthenticator.php
+++ src/Security/AppAthenticatorAuthenticator.php
@@ -48,7 +48,7 @@ class AppAthenticatorAuthenticator extends AbstractLoginFormAuthenticator
 
         // For example:
         // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        return new RedirectResponse($this->urlGenerator->generate('admin'));
+        return new RedirectResponse($this->urlGenerator->generate('app_home'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
